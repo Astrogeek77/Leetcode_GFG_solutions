664c1bd9b1cc7e292efcc88dad1bcfd431b95f9c
Gautam Jain <astrogeek77@outlook.com>
2022-04-11 18:07:27 +0530

    Time: 12 ms (98.14%), Space: 19.5 MB (80.46%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int ans;
+        // Moore Voting Algo
+        int count = 0;
+        int candidate = 0;
+        int n = nums.size();
         
-        for(int i = 0; i< nums.size(); i++)
-            m[nums[i]]++;
-       
-        
-        for(int i = 0; i< nums.size(); i++)
-        {
-            if(m[nums[i]] > (nums.size()/2))
-            {
-                ans =  nums[i];
-                break;
-            }
+        for(int i = 0; i < n; i++){
+            if(count == 0) candidate = nums[i];
+            
+            if(candidate == nums[i]) count++;
+            else count--;
         }
-        return ans; 
+        return candidate;
     }
 };
