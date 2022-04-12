b67b50916adee3345c67348f1b65ba5c880f3634
Gautam Jain <astrogeek77@outlook.com>
2022-04-12 18:09:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (60.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -16,12 +16,11 @@ public:
     int dp[101][101];
     int solve(int m,int n){
        if(m<0 || n<0) return 0;
-        else if(m==0  || n==0) return 1;
-         else if(dp[m][n]>0) return dp[m][n];
-        else{
-            return dp[m][n]=solve(m-1,n) +solve(m,n-1); 
-        }
+       if(m==0  || n==0) return 1;
+       if(dp[m][n]> 0) return dp[m][n];
+       else return dp[m][n]=solve(m-1,n) +solve(m,n-1); 
     }
+    
     int uniquePaths(int m, int n) {
         return solve(m-1,n-1);
     }
