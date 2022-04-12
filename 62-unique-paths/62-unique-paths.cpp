4ca60cfeedd1aec354ae573cccb5582fe004fb86
Gautam Jain <astrogeek77@outlook.com>
2022-04-12 18:24:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,18 +10,46 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int dp[101][101];
+//     int solve(int m,int n){
+//        if(m<0 || n<0) return 0;
+//        if(m==0  || n==0) return 1;
+//        if(dp[m][n]> 0) return dp[m][n];
+//        else return dp[m][n]=solve(m-1,n) +solve(m,n-1); 
+//     }
+    
+//     int uniquePaths(int m, int n) {
+//         return solve(m-1,n-1);
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     // Combinoritics    
+//     int uniquePaths(int m, int n) {
+//  //         Total Directions = m + n - 2;
+//         int total = m + n - 2;
+//         double result = 1;
+        
+//         for(int i = 1; i <= (m - 1); i++){
+//             result *= (total - (m - 1) + i)/ i;
+//         }
+//         return (int)result;
+//     }
+// };
+
 class Solution {
 public:
-    // int mod=2e9;
-    int dp[101][101];
-    int solve(int m,int n){
-       if(m<0 || n<0) return 0;
-       if(m==0  || n==0) return 1;
-       if(dp[m][n]> 0) return dp[m][n];
-       else return dp[m][n]=solve(m-1,n) +solve(m,n-1); 
-    }
-    
     int uniquePaths(int m, int n) {
-        return solve(m-1,n-1);
+            int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
     }
 };
