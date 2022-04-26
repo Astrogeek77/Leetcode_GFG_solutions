4491e0d31bf4a474380e3ab54b75aea9a23d8358
Gautam Jain <astrogeek77@outlook.com>
2022-04-26 21:18:01 +0530

    Time: 3 ms (59.48%), Space: 7.6 MB (72.03%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* curr;
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* ans = new TreeNode(0);
+        curr = ans;
+        inorder(root);
+        return ans->right;
+    }
+   
+
+   void inorder(TreeNode* node) {
+        if (node == NULL) return;
+        inorder(node->left);
+        node->left = NULL;
+        curr->right = node;
+        curr = node;
+        inorder(node->right);
+    }
+};
