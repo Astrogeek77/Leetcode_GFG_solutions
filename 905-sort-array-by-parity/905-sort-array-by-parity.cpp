effbd7a9ec7be1201a2441eac1941796a5a975f4
Gautam Jain <astrogeek77@outlook.com>
2022-05-02 09:37:59 +0530

    Time: 17 ms (35.77%), Space: 16.3 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         stack<int> st;
+//         int n = nums.size();
+//         vector<int> ans;
+        
+//         for(int i = 0; i < n; i++){
+//             if(nums[i] % 2 != 0) st.push(nums[i]);
+//         }
+        
+//         for(int i = 0; i < n; i++){
+//             if(nums[i] % 2 == 0) st.push(nums[i]);
+//         }
+        
+//         while(!st.empty()){
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        stack<int> st;
+        int next = 0;
         int n = nums.size();
-        vector<int> ans;
         
         for(int i = 0; i < n; i++){
-            if(nums[i] % 2 != 0) st.push(nums[i]);
+            if(nums[i] % 2 == 0){
+                swap(nums[i], nums[next++]);
+            }
         }
-        
-        for(int i = 0; i < n; i++){
-            if(nums[i] % 2 == 0) st.push(nums[i]);
-        }
-        
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        return ans;
+        return nums;
     }
 };
