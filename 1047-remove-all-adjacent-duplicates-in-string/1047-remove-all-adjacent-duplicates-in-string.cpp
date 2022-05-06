6fec0595244f716c36bffb5e12de561c29ad2803
Gautam Jain <astrogeek77@outlook.com>
2022-05-06 22:11:46 +0530

    Time: 38 ms (36.21%), Space: 18.9 MB (8.78%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+        string removeDuplicates(string s)
+        {
+            int k = 2; // we take = 2 here
+            vector<pair<char, int>> count; // declare a vector to store a pair of char and int (works like a stack here.)
+
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (count.empty() || s[i] != count.back().first) count.push_back({ s[i],
+                    1 }); // if s[i] != s[i-1] we push the char with 1 into vector
+                else if (++(count.back().second) == k) count.pop_back(); // if any double is found we pop_back()
+            }
+
+            s = ""; // we contruct the string again with remaining chars
+            for (auto &x: count) s += string(x.second, x.first);
+
+            return s;
+        }
+};
