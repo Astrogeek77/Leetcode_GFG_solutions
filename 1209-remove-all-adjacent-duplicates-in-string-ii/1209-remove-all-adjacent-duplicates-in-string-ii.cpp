e8c0ebae490cc73ed55a3b28ca2e7564dd47a8ff
Gautam Jain <astrogeek77@outlook.com>
2022-05-06 22:20:24 +0530

    Time: 421 ms (5.59%), Space: 9.3 MB (97.86%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -7,12 +7,12 @@ class Solution
         {
             for (int i = 0; i < s.size(); i++)
             {
-                if (i == 0 || s[i] != s[i - 1]) st.push(1);
-                else if (++st.top() == k)
+                if (i == 0 || s[i] != s[i - 1]) st.push(1);  // push 1 to the stack for every new char
+                else if (++st.top() == k) // if the count of character reaches k
                 {
-                    st.pop();
-                    s.erase(i - k + 1, k);
-                    i = i - k;
+                    st.pop(); // pop off the stack
+                    s.erase(i - k + 1, k); // erase that portion of string
+                    i = i - k; // reset i
                 }
             }
             return s;
