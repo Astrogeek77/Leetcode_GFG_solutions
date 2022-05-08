665a6db24deeeaed621e381bb6021d08dbce0efa
Gautam Jain <astrogeek77@outlook.com>
2022-05-08 13:39:00 +0530

    Time: 58 ms (55.88%), Space: 14.6 MB (32.85%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,28 +1,46 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode(int x) : val(x), next(NULL) {}
+ *  int val;
+ *  ListNode * next;
+ *  ListNode(int x) : val(x), next(NULL) {}
  *};
  */
+// class Solution
+// {
+//     public:
+//         ListNode* getIntersectionNode(ListNode *headA, ListNode *headB)
+//         {
+//            	// ListNode *curr2 = headB;
+
+//             while (headB)
+//             {
+//                 ListNode *curr1 = headA;
+//                 while (curr1)
+//                 {
+//                     if (curr1 == headB) return headB;
+//                     curr1 = curr1->next;
+//                 }
+//                 headB = headB->next;
+//             }
+//             return NULL;
+//         }
+// };
+
 class Solution
 {
     public:
-        ListNode* getIntersectionNode(ListNode *headA, ListNode *headB)
+        ListNode* getIntersectionNode(ListNode *head1, ListNode *head2)
         {
-           	// ListNode *curr2 = headB;
+            ListNode *curr1 = head1;
+            ListNode *curr2 = head2;
 
-            while (headB)
+            while (curr1 != curr2)
             {
-                ListNode *curr1 = headA;
-                while (curr1)
-                {
-                    if (curr1 == headB) return headB;
-                    curr1 = curr1->next;
-                }
-                headB = headB->next;
+                curr1 = curr1 == NULL ? head2 : curr1->next;
+                curr2 = curr2 == NULL ? head1 : curr2->next;
             }
-            return NULL;
+
+            return curr1;
         }
 };
