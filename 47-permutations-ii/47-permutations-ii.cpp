ce029688d5430b3d0990d8fae13ab48c9b69cc7d
Gautam Jain <astrogeek77@outlook.com>
2022-05-12 22:57:45 +0530

    Time: 74 ms (21.11%), Space: 9.9 MB (35.75%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,38 +1,67 @@
+// class Solution
+// {
+// public:
+//     unordered_map<int, int> map;
+//     vector<int> temp;
+    
+//     void func(int i, vector<int> &nums, vector<vector<int>> &ans)
+//     {
+//         if (i == nums.size())
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         for (auto k : map)
+//         {
+//             int key = k.first;
+//             int value = k.second;
+            
+//             if(value == 0) continue;   
+//             temp.push_back(key);
+//             map[key]--;
+            
+//             func(i + 1, nums, ans);
+            
+//             temp.pop_back();
+//             map[key]++;
+//         }
+//     }
+
+//     vector<vector<int>> permuteUnique(vector<int> &nums)
+//     {
+//         vector<vector<int>> ans;
+//         for(auto x: nums) map[x]++;
+        
+//         func(0, nums, ans);
+//         return ans;
+//     }
+// };
+
 class Solution
 {
 public:
-    unordered_map<int, int> map;
-    vector<int> temp;
+    vector<vector<int>> ans;
+    set<vector<int>> set;
     
-    void func(int i, vector<int> &nums, vector<vector<int>> &ans)
+    void helper(vector<int> &nums, int i, int j)
     {
-        if (i == nums.size())
-        {
-            ans.push_back(temp);
+        if(i == nums.size() and set.find(nums) == set.end()){
+            ans.push_back(nums);
+            set.insert(nums);
             return;
         }
-        for (auto k : map)
-        {
-            int key = k.first;
-            int value = k.second;
-            
-            if(value == 0) continue;   
-            temp.push_back(key);
-            map[key]--;
-            
-            func(i + 1, nums, ans);
-            
-            temp.pop_back();
-            map[key]++;
+        
+        for(int k = i; k <= j; k++){
+            swap(nums[k], nums[i]);
+            helper(nums, i+1, j);
+            swap(nums[k], nums[i]);
         }
+        
     }
 
     vector<vector<int>> permuteUnique(vector<int> &nums)
     {
-        vector<vector<int>> ans;
-        for(auto x: nums) map[x]++;
-        
-        func(0, nums, ans);
+        helper(nums, 0, nums.size() - 1);
         return ans;
     }
 };
