0cb46d0e9d8bca8bdb3a3ca5f1fe74639b65d980
Gautam Jain <astrogeek77@outlook.com>
2022-05-22 16:20:33 +0530

    Added solution - LeetHub

---
+++ Find an Replace in String - GFG/find-an-replace-in-string.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string findAndReplace(string S ,int Q, int index[], string sources[], string targets[]) {
+        for(int i = Q-1; i >= 0; i--)
+        {
+           int j = index[i];
+           int len = sources[i].size();
+           
+           if(S.substr(j, len) == sources[i])
+           {
+              S = S.substr(0,j) +  targets[i] + S.substr(j + len); 
+              // string till starting index + target string + string after string index
+           }
+       }
+       return S;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin>>S;
+        
+        int Q;
+        cin>>Q;
+        int index[Q];
+        string sources[Q], targets[Q];
+        
+        for(int i=0; i<Q; i++)
+            cin>>index[i];
+        for(int i=0; i<Q; i++)
+            cin>>sources[i];
+        for(int i=0; i<Q; i++)
+            cin>>targets[i];
+
+        Solution ob;
+        cout<<ob.findAndReplace(S,Q,index,sources,targets)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
