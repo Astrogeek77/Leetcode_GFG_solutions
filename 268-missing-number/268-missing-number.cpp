7decf5e287c5676b4de62d66183de80d16592890
Gautam Jain <astrogeek77@outlook.com>
2022-05-28 15:32:28 +0530

    Time: 25 ms (59.12%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,12 +1,25 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size(), sum1 = 0;
-        
-        for(auto x: nums) sum1 += x;
-        
-        int sum2 = (n * (n+1)/2);
-        
-        return sum2 - sum1;
-    }
+// class Solution
+// {
+//     public:
+//         int missingNumber(vector<int> &nums)
+//         {
+//             int n = nums.size(), sum1 = 0;
+
+//             for (auto x: nums) sum1 += x;
+
+//             int sum2 = (n *(n + 1) / 2);
+
+//             return sum2 - sum1;
+//         }
+// };
+
+class Solution
+{
+    public:
+        int missingNumber(vector<int> &nums)
+        {
+            int target = nums.size() *(nums.size() + 1) / 2;
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            return target - sum;
+        }
 };
