dbb00cf24a0b3738edc58f0615cdd9b986e597be
Gautam Jain <astrogeek77@outlook.com>
2022-05-30 19:16:31 +0530

    Added solution - LeetHub

---
+++ Longest Path in a matrix - GFG/longest-path-in-a-matrix.cpp
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+       	// check limits for i and j (func)
+        bool checkSafe(int i, int j, vector<vector < int>> &matrix)
+        {
+            int n = matrix.size();	// rows
+            int m = matrix[0].size();	// columns
+            return (i >= 0 and i < n and j >= 0 and j < m);
+        }
+   	// helper recursive func
+    int helper(int i, int j, vector<vector < int>> &matrix, vector<vector < int>> &dp, int n, int m)
+    {
+        if(!checkSafe(i, j, matrix)) return 0; // base case
+        if(dp[i][j] != -1) return dp[i][j]; // Tabulate
+        
+        int up = 0, down = 0, left = 0, right = 0;
+        
+        // up
+        if(checkSafe(i-1, j, matrix) and matrix[i-1][j] > matrix[i][j])
+            up = helper(i-1, j, matrix, dp, n, m);
+        
+        // down
+        if(checkSafe(i+1, j, matrix) and matrix[i+1][j] > matrix[i][j])
+            down = helper(i+1, j, matrix, dp, n, m);
+        
+        // left
+        if(checkSafe(i, j-1, matrix) and matrix[i][j-1] > matrix[i][j])
+            left = helper(i, j-1, matrix, dp, n, m);
+        
+        // right
+        if(checkSafe(i, j+1, matrix) and matrix[i][j+1] > matrix[i][j])
+            right = helper(i, j+1, matrix, dp, n, m);
+        
+        return dp[i][j] = 1 + max(up, max(down, max(left, right)));
+    }
+
+    int longestIncreasingPath(vector<vector < int>> &matrix) {
+        int n = matrix.size();	// rows
+        int m = matrix[0].size();	// columns
+        
+        int result = 1; // answer
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                // if(dp[i][j] == -1)
+                    result = max(result, helper(i, j, matrix, dp, n, m));
+            }
+        }
+        return result;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>matrix(n, vector<int>(m, 0));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)cin >> matrix[i][j];
+		Solution obj;
+		int ans = obj.longestIncreasingPath(matrix);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
