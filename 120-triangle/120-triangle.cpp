39c5e80a7a7d8a9cb523f1346c7a71fd9a1f53d7
Gautam Jain <astrogeek77@outlook.com>
2022-06-13 10:41:40 +0530

    Time: 4 ms (89.10%), Space: 8.9 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,50 +1,54 @@
-class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& tri) {
-        int n = tri.size();
-        vector<int> next(n, -1);
-        for(int j=0;j<n;j++) next[j] = tri[n-1][j];
+class Solution
+{
+    public:
+        int helper(int i, int j, vector<vector < int>> &tri, int n, vector< vector< int>> &dp)
+        {
+            if (i == n - 1) return tri[i][j];
+            if (dp[i][j] != -1) return dp[i][j];
 
-        for(int i=n-2; i>=0; i--){
-            vector<int> curr(n, 0);
-            for(int j=i; j>=0; j--){
-
-                int down = tri[i][j] + next[j];
-                int diagonal = tri[i][j] + next[j+1];
-
-                curr[j] = min(down, diagonal);
-            }
-            next = curr;
+            int dn = tri[i][j] + helper(i + 1, j, tri, n, dp);
+            int diag = tri[i][j] + helper(i + 1, j + 1, tri, n, dp);
+            return dp[i][j] = min(dn, diag);
         }
-        return next[0];
+
+    int minimumTotal(vector<vector < int>> &tri)
+    {
+        int n = tri.size();
+        vector<vector < int>> dp(n, vector<int> (n, -1));
+        return helper(0, 0, tri, n, dp);
     }
 };
 
-// int helper(int i, int j, vector<vector<int>> &tri, int n, vector<vector<int>> &dp){
-// 	if(i == n - 1) return tri[i][j];
-// 	if(dp[i][j] != -1) return dp[i][j];
-	
-// 	int dn = tri[i][j] + helper(i+1, j, tri, n, dp);
-// 	int diag = tri[i][j] + helper(i+1, j+1, tri, n, dp);
-// 	return dp[i][j] = min(dn, diag);
-// }
+// int minimumTotal(vector<vector<int>>& tri) {
+//         int n = tri.size();
+//         vector<int> next(n, -1);
+//         for(int j=0;j < n;j++) next[j] = tri[n-1][j];
 
-// int minimumPathSum(vector<vector<int>>& tri, int n){
-// 	vector<vector<int>> dp(n, vector<int> (n, -1));
-// 	return helper(0, 0, tri, n, dp);
-// }
+//         for(int i=n-2; i>=0; i--){
+//             vector<int> curr(n, 0);
+//             for(int j=i; j>=0; j--){
+
+//                 int down = tri[i][j] + next[j];
+//                 int diagonal = tri[i][j] + next[j+1];
+
+//                 curr[j] = min(down, diagonal);
+//             }
+//             next = curr;
+//         }
+//         return next[0];
+//     }
 
 // Tabulation
 // int minimumPathSum(vector<vector<int>>& tri, int n){
-// 	vector<vector<int>> dp(n, vector<int> (n, -1));
-// 	for(int j=0;j<n;j++) dp[n-1][j] = tri[n-1][j];
-	
+// 	vector<vector < int>> dp(n, vector<int> (n, -1));
+// 	for(int j=0;j < n;j++) dp[n-1][j] = tri[n-1][j];
+
 // 	for(int i=n-2; i>=0; i--){
 //         for(int j=i; j>=0; j--){
-            
+
 //             int down = tri[i][j]+dp[i+1][j];
 //             int diagonal = tri[i][j]+dp[i+1][j+1];
-            
+
 //             dp[i][j] = min(down, diagonal);
 //         }
 //     }
@@ -54,15 +58,15 @@ public:
 // Space Optimization
 // int minimumPathSum(vector<vector<int>>& tri, int n){
 // 	vector<int> next(n, 0);
-// 	for(int j=0;j<n;j++) next[j] = tri[n-1][j];
-	
+// 	for(int j=0;j < n;j++) next[j] = tri[n-1][j];
+
 // 	for(int i=n-2; i>=0; i--){
 // 		vector<int> curr(n, 0);
 //         for(int j=i; j>=0; j--){
-            
+
 //             int down = tri[i][j] + next[j];
 //             int diagonal = tri[i][j] + next[j+1];
-            
+
 //             curr[j] = min(down, diagonal);
 //         }
 // 		next = curr;
