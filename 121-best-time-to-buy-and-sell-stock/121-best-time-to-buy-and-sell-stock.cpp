c875b5c69e7ccb329093217f4ef330854e365b81
Gautam Jain <astrogeek77@outlook.com>
2022-06-14 17:08:00 +0530

    Time: 236 ms (16.30%), Space: 93.2 MB (89.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,22 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& a) {
-       
-        int min_cp=INT_MAX;
-        int profit=INT_MIN;
-           
-        for(int i=0;i<a.size();i++)
+class Solution
+{
+    public:
+        int maxProfit(vector<int> &a)
         {
-		//logic
-            if(a[i]<min_cp)
-                min_cp=a[i];
-            
-            if(a[i]-min_cp>profit)
-                profit=a[i]-min_cp;
+
+            int min_cp = INT_MAX;
+            int profit = INT_MIN;
+
+            for (int i = 0; i < a.size(); i++)
+            {
+               	//logic
+                if (a[i] < min_cp)
+                    min_cp = a[i];
+
+                if (a[i] - min_cp > profit)
+                    profit = a[i] - min_cp;
+            }
+
+            return profit;
         }
-        
-        return profit;
-        
-    }
 };
