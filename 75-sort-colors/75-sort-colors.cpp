c9b6fd957b24450275b178ebb51710c967f6f31e
Gautam Jain <astrogeek77@outlook.com>
2022-07-23 14:25:34 +0530

    Time: 7 ms (17.15%), Space: 8.1 MB (90.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,28 +1,32 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-       int lo = 0; 
-        int hi = nums.size() - 1; 
-        int mid = 0; 
+class Solution
+{
+    public:
+        void sortColors(vector<int> &nums)
+        {
+            int lo = 0;
+            int hi = nums.size() - 1;
+            int mid = 0;
 
-        while (mid <= hi) { 
-            switch (nums[mid]) { 
+            while (mid <= hi)
+            {
+                switch (nums[mid])
+                {
 
-            // If the element is 0 
-            case 0: 
-                swap(nums[lo++], nums[mid++]); 
-                break; 
+                   	// If the element is 0 
+                    case 0:
+                        swap(nums[lo++], nums[mid++]);
+                        break;
 
-            // If the element is 1 . 
-            case 1: 
-                mid++; 
-                break; 
+                       	// If the element is 1 . 
+                    case 1:
+                        mid++;
+                        break;
 
-            // If the element is 2 
-            case 2: 
-                swap(nums[mid], nums[hi--]); 
-                break; 
+                       	// If the element is 2 
+                    case 2:
+                        swap(nums[mid], nums[hi--]);
+                        break;
+                }
             }
         }
-    }
 };
