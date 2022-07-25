ecb73035f631f618d6f66881b8ead320d4cd82b8
Gautam Jain <astrogeek77@outlook.com>
2022-07-25 16:40:35 +0530

    Time: 28 ms (71.67%), Space: 23.1 MB (76.25%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,6 +3,7 @@ class Solution
     public:
         void reverseString(vector<char> &s)
         {
+            // swap the elements till the half of the vector size
             int n = s.size();
             for (int i = 0; i < n / 2; i++)
             {
