66fe41ba35d81e2bcab09069d3b16411e82462ee
Gautam Jain <astrogeek77@outlook.com>
2022-07-25 16:06:01 +0530

    Time: 38 ms (48.35%), Space: 19.3 MB (13.43%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,6 +3,7 @@ class Solution
     public:
         void moveZeroes(vector<int> &nums)
         {
+           	//if we find any non zero elmt in the array, swap it with front index elmt and update both front, curr indexes. Else just update curr index.
             for (int i = 0, j = 0; j < nums.size(); j++)
             {
                 if (nums[j] != 0)
