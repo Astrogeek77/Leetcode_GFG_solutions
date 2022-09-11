c4933aa0970223dddab545fb2a4a9cb3c8b9310d
Gautam Jain <astrogeek77@outlook.com>
2022-09-11 21:03:00 +0530

    Time: 105 ms (5.37%), Space: 27.6 MB (63.27%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+        int search(vector<int> &nums, int target)
+        {
+            int hi = nums.size() - 1;
+            int lo = 0;
+
+            while (lo <= hi)
+            {
+                int mid = (hi + lo) / 2;
+
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] > target) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            return -1;
+        }
+};
