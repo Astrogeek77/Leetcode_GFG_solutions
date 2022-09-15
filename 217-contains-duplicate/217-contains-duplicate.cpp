c166a221f3f21d8c47b97ce06d04cab96c9d3349
Gautam Jain <astrogeek77@outlook.com>
2022-09-15 19:36:33 +0530

    Time: 203 ms (19.20%), Space: 51.5 MB (52.37%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,8 +1,25 @@
+// class Solution
+// {
+//     public:
+//         bool containsDuplicate(vector<int> &nums)
+//         {
+//             return nums.size() > set<int> (nums.begin(), nums.end()).size();
+//         }
+// };
+
 class Solution
 {
     public:
         bool containsDuplicate(vector<int> &nums)
         {
-            return nums.size() > set<int> (nums.begin(), nums.end()).size();
+            unordered_map<int, int> map;
+
+            for (auto x: nums)
+            {
+                if (map.find(x) != map.end()) return true;
+                map[x]++;
+            }
+
+            return false;
         }
 };
