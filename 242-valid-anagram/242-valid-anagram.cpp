d0f8e3d0fb1b8cd47caf00720e0ab0f9dc76e0be
Gautam Jain <astrogeek77@outlook.com>
2022-09-20 20:22:45 +0530

    Time: 28 ms (12.94%), Space: 7.4 MB (49.47%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
-        }
-        
-        map<char, int> tmap;
-        map<char, int> smap;
-        
-        for(auto ch : s) smap[ch]++;
-        for(auto ch : t) tmap[ch]++;
-        
-        for(auto x : smap){
-            char ch = x.first;
-            int num = x.second;
-            
-            if(tmap[ch] != num) return false;
-        }
-     return true;       
+class Solution
+{
+    public:
+        bool isAnagram(string s, string t)
+        {
+            if (s.size() != t.size()) return false;
 
-    }
+            map<char, int> tmap;
+            map<char, int> smap;
+
+            for (auto ch: s) smap[ch]++;
+            for (auto ch: t) tmap[ch]++;
+
+            for (auto x: smap)
+            {
+                char ch = x.first;
+                int num = x.second;
+
+                if (tmap[ch] != num) return false;
+            }
+            return true;
+        }
 };
