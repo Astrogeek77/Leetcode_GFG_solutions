99363546ce27476a7f907dc251458f828c1fc37a
Gautam Jain <astrogeek77@outlook.com>
2022-09-26 15:17:47 +0530

    Time: 7 ms (85.05%), Space: 10.2 MB (72.46%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    vector<string> res;
+    char changeCase(char c)
+    {
+        if (islower(c)) return toupper(c);
+        else return tolower(c);
+    }
+    void backtrack(string temp, int idx)
+    {
+        res.push_back(temp);
+
+        for (int i = idx; i < temp.size(); ++i)
+        {
+            if (isalpha(temp[i]))
+            {
+                temp[i] = changeCase(temp[i]);
+                backtrack(temp, i + 1);
+                temp[i] = changeCase(temp[i]);
+            }
+        }
+    }
+    public:
+        vector<string> letterCasePermutation(string s)
+        {
+            backtrack(s, 0);
+            return res;
+        }
+};
