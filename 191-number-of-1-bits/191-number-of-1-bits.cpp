a675200592e9b295425e251e51737b1b6c9bbcac
Gautam Jain <astrogeek77@outlook.com>
2022-09-28 14:25:19 +0530

    Time: 3 ms (63.04%), Space: 5.9 MB (78.77%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,14 @@
-class Solution {
-public:
-    int hammingWeight(uint32_t n) {
-        int ans = 0;
-        while(n > 0)
+class Solution
+{
+    public:
+        int hammingWeight(uint32_t n)
         {
-            n = n & (n-1);
-            ans++;;
+            int ans = 0;
+            while (n > 0)
+            {
+                n = n & (n - 1);
+                ans++;;
+            }
+            return ans;
         }
-        return ans;
-    }
 };
