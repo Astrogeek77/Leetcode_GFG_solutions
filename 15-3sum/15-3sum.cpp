0d39dea97507826b77b6e67fcbad4a1583b9bf08
Gautam Jain <astrogeek77@outlook.com>
2022-10-09 17:42:31 +0530

    Time: 94 ms (89.91%), Space: 19.8 MB (95.49%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,34 +1,41 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-       sort(nums.begin(), nums.end());
-        
-       if(nums.size() < 3)
-        return {};
-        
-       vector<vector<int>> ans;
-        
-       for(int i = 0; i < nums.size() - 2; i++){
-           if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
-               int lo = i + 1, hi = nums.size() - 1;
-               
-               while(lo < hi){
-                   int sum = nums[i] + nums[lo] + nums[hi];
-                   
-                   if(sum == 0){
-                      ans.push_back({nums[i], nums[lo], nums[hi]});
-                       
-                      while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
-                      while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
-                       
-                      lo++;
-                      hi--; 
-                   } 
-                  else if (sum < 0) lo++;
-                  else hi--; 
-               }
-           }
-       }
-        return ans;
-    }
+class Solution
+{
+    public:
+        vector<vector < int>> threeSum(vector<int> &nums)
+        {
+            sort(nums.begin(), nums.end());
+
+            if (nums.size() < 3) return {};
+
+            vector<vector < int>> ans;
+
+            for (int i = 0; i < nums.size() - 2; i++)
+            {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                {
+                    int lo = i + 1, hi = nums.size() - 1;
+
+                    while (lo < hi)
+                    {
+                        int sum = nums[i] + nums[lo] + nums[hi];
+
+                        if (sum == 0)
+                        {
+                            ans.push_back({ nums[i],
+                                nums[lo],
+                                nums[hi] });
+
+                            while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                            while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+
+                            lo++;
+                            hi--;
+                        }
+                        else if (sum < 0) lo++;
+                        else hi--;
+                    }
+                }
+            }
+            return ans;
+        }
 };
