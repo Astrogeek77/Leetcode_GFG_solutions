86582258c4f928840686cb811bad0f8f40f1bca1
Gautam Jain <astrogeek77@outlook.com>
2022-10-10 09:50:33 +0530

    Time: 192 ms (5.03%), Space: 24.8 MB (5.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,34 +1,66 @@
 class Solution
 {
     public:
-
-        void turn_to_dust(vector<vector < char>> &grid, int i, int j, int m, int n)
+        int numIslands(vector<vector < char>> &grid)
         {
-            if (i < 0 || j < 0 || i == m || j == n || grid[i][j] == '0') return;
-            grid[i][j] = '0';
-            turn_to_dust(grid, i + 1, j, m, n);
-            turn_to_dust(grid, i, j + 1, m, n);
-            turn_to_dust(grid, i - 1, j, m, n);
-            turn_to_dust(grid, i, j - 1, m, n);
-            return;
-        }
 
-    int numIslands(vector<vector < char>> &grid)
-    {
-        int m = grid.size();
-        int n = grid[0].size();
-        int res = 0;
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
+            unordered_map<string, string> dsu;
+
+            int M = grid.size();
+            int N = grid[0].size();
+
+            for (int i = 0; i < M; i++)
             {
-                if (grid[i][j] == '1')
+                for (int j = 0; j < N; j++)
                 {
-                    res++;
-                    turn_to_dust(grid, i, j, m, n);
+                    if (grid[i][j] == '1')
+                    {
+                        string a = to_string(i) + "," + to_string(j);
+                        dsu[a] = a;
+                       	// Check left
+                        if (j - 1 >= 0 && grid[i][j - 1] == '1')
+                        {
+                            string b = to_string(i) + "," + to_string(j - 1);
+                            Union(a, b, dsu);
+                        }
+                       	// Check Up
+                        if (i - 1 >= 0 && grid[i - 1][j] == '1')
+                        {
+                            string b = to_string(i - 1) + "," + to_string(j);
+                            Union(a, b, dsu);
+                        }
+                    }
                 }
             }
+
+            int numOfIslands = 0;
+            for (auto &it: dsu)
+            {
+                if (it.second == it.first)
+                {
+                    numOfIslands++;
+                }
+            }
+
+            return numOfIslands;
         }
-        return res;
+
+    string find(string cur, unordered_map<string, string> &dsu)
+    {
+        if (dsu[cur] == cur)
+        {
+            return cur;
+        }
+        return find(dsu[cur], dsu);
+    }
+
+    void Union(string &a, string &b, unordered_map<string, string> &dsu)
+    {
+        string parent = find(a, dsu);
+        string child = find(b, dsu);
+        if (parent != child)
+        {
+            dsu[parent] = child;
+        }
     }
 };
