983b37a5d21277505f9a1c052fb75c62d0d24f32
Gautam Jain <astrogeek77@outlook.com>
2022-10-11 20:19:43 +0530

    Time: 1010 ms (13.90%), Space: 0B (100.00%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,3 @@
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y' 
+
+# https://leetcode.com/problems/recyclable-and-low-fat-products/discuss/1062936/SQL-1-liner-solution-(This-is-a-FB-DE-interview-question)
