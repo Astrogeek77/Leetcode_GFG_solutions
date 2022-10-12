3d2350cf50b293a6d997660e29c4e7b7d5ae373e
Gautam Jain <astrogeek77@outlook.com>
2022-10-12 10:20:33 +0530

    Time: 290 ms (21.54%), Space: 14.8 MB (88.11%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,15 +3,15 @@ class Solution
     public:
         bool searchMatrix(vector<vector < int>> &matrix, int target)
         {
-            int m = matrix.size(), n = m ? matrix[0].size() : 0;
-            int r = 0, c = n - 1;
-            while (r < m && c >= 0)
+            int n = matrix.size();
+            int m = matrix[0].size();
+          
+            int lo = 0, hi = m - 1;
+            while (lo < n and hi >= 0)
             {
-                if (matrix[r][c] == target)
-                {
-                    return true;
-                }
-                matrix[r][c] > target ? c-- : r++;
+                if (matrix[lo][hi] == target) return true;
+                else if (matrix[lo][hi] > target) hi--;
+                else lo++;
             }
             return false;
         }
