9e59009224a7769d4325842e731f88383c6e17f8
Gautam Jain <astrogeek77@outlook.com>
2022-10-26 14:00:01 +0530

    Time: 15 ms (5.25%), Space: 5.8 MB (90.51%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,29 @@
+// class Solution
+// {
+//     public:
+//         int integerBreak(int n)
+//         {
+//             if (n <= 3) return n - 1;
+//             else if (n % 3 == 0) return pow(3, n / 3);
+//             else if (n % 3 == 1) return 4* pow(3, (n - 4) / 3);
+//             else return 2* pow(3, n / 3);
+//         }
+// };
+
+class Solution
+{
+    public:
+        int f(int i, int sum)
+        {
+            if (i == 1) return 1;
+            int notpick = f(i - 1, sum);
+            int pick = 0;
+            if (sum >= i) pick = i* f(i, sum - i);
+            return max(pick, notpick);
+        }
+
+    int integerBreak(int n)
+    {
+        return f(n - 1, n);
+    }
+};
