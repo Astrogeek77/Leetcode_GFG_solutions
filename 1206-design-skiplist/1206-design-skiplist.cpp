f7e5f344b2976ea23ba6c7cfd01d97dc56a5ff79
Gautam Jain <astrogeek77@outlook.com>
2022-11-08 19:06:13 +0530

    Time: 137 ms (61.88%), Space: 30.2 MB (53.59%) - LeetHub

---
+++ 1206-design-skiplist/1206-design-skiplist.cpp
@@ -0,0 +1,103 @@
+class Skiplist
+{
+    class node
+    {
+        public:
+        int val;
+        node * next;
+        node * down;
+       	// constructor for initializing node val and next, down pointers
+        node(int _val, node *_next, node *_down)
+        {
+            val = _val;
+            next = _next;
+            down = _down;
+        }
+    };
+
+    public:
+    node * head;
+    vector<node*> insertionPoints;
+
+    Skiplist()
+    {
+        head = new node(0, nullptr, nullptr);
+    }
+
+    bool search(int target)
+    {
+       	// traverse the express lane and stop on the 
+       	// node whose next is larger than the target.
+
+        node *curr = head;
+        while (curr)
+        {
+           	// travel through express lane until node's next is larger than target
+            while (curr->next and curr->next->val < target) curr = curr->next;
+
+           	// then traverse to deeper layer if such node is found or no next node exists.
+            if (!curr->next or curr->next->val > target) curr = curr->down;
+
+           	// if target is found
+            else if (curr->val == target or curr->next->val == target) return true;
+        }
+        return false;
+    }
+
+    void add(int num)
+    {
+        insertionPoints.clear();
+        node *curr = head;
+
+        while (curr)
+        {
+            while (curr->next and curr->next->val < num) curr = curr->next;
+            insertionPoints.push_back(curr);
+            curr = curr->down;
+        }
+
+        node *downNode = nullptr;
+        bool insertUp = true;
+
+        while (insertUp and insertionPoints.size() > 0)
+        {
+            node *dummy = insertionPoints.back();
+            insertionPoints.pop_back();
+
+            dummy->next = new node(num, dummy->next, downNode);
+            downNode = dummy->next;
+
+            insertUp = (rand() &1) == 0;
+        }
+        if (insertUp)
+        {
+            head = new node(0, new node(num, NULL, downNode), head);
+        }
+    }
+
+    bool erase(int num)
+    {
+        node *curr = head;
+        bool seen = false;
+        while (curr)
+        {
+            while (curr->next and curr->next->val < num) curr = curr->next;
+            if (!curr->next or curr->next->val > num) curr = curr->down;
+            else
+            {
+                seen = true;
+                curr->next = curr->next->next;
+                curr = curr->down;
+            }
+        }
+        return seen;
+    }
+};
+
+/**
+ *Your Skiplist object will be instantiated and called as such:
+ *Skiplist* obj = new Skiplist();
+ *bool param_1 = obj->search(target);
+ *obj->add(num);
+ *bool param_3 = obj->erase(num);
+ */
