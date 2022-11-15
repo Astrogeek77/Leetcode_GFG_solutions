f8617a3eb540319c2cec126537ad50afe1d311b9
Gautam Jain <astrogeek77@outlook.com>
2022-11-15 10:32:49 +0530

    Time: 43 ms (82.20%), Space: 31 MB (9.34%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -9,22 +9,53 @@
  *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
+// class Solution
+// {
+//     public:
+//     void preorder(TreeNode *root, int &count)
+//     {
+//         if (!root) return;
+
+//         count++;
+//         preorder(root->left, count);
+//         preorder(root->right, count);
+//     }
+
+//     int countNodes(TreeNode *root)
+//     {
+//         int count = 0;
+//         preorder(root, count);
+//         return count;
+//     }
+// };
+
 class Solution
 {
     public:
-    void preorder(TreeNode *root, int &count)
-    {
-        if (!root) return;
-
-        count++;
-        preorder(root->left, count);
-        preorder(root->right, count);
-    }
-
     int countNodes(TreeNode *root)
     {
-        int count = 0;
-        preorder(root, count);
-        return count;
+        if(!root) return 0;
+        // left and right heights
+        int leftH = 0, rightH = 0;
+        // left and right nodes
+        TreeNode *leftN = root, *rightN = root;
+        
+        // traverse through left
+        while(leftN)
+        {
+            leftH++;
+            leftN = leftN->left;
+        }     
+        
+        // traverse through right
+        while(rightN)
+        {
+            rightH++;
+            rightN = rightN->right;
+        }   
+        
+        // use complete binary tree property for evry complete node
+        if(leftH == rightH) return pow(2, leftH) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
