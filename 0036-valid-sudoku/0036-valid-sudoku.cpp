fe66a19e72d2baaeb456092a5990e8ab1b4e813b
Gautam Jain <astrogeek77@outlook.com>
2022-11-23 09:51:53 +0530

    Time: 54 ms (36.03%), Space: 18.1 MB (72.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,11 +1,14 @@
 class Solution
 {
-    bool check(int row, int col, vector<vector<char>> &board, int val)
+    bool check(int row, int col, vector<vector<char>> &board, char val)
     {
+        cout << val << endl;
         for(int k = 0; k < 9; k++)
         {
+            // check row and col
             if(board[row][k] == val) return false;
             if(board[k][col] == val) return false;
+            // to check the corresponding 3 x 3 square.
             if(board[(row / 3) * 3 + k/3][(col/3) * 3 + k % 3] == val) return false; 
         }
         return true;
@@ -23,9 +26,12 @@ class Solution
                     if(board[i][j] != '.')
                     {
                         char val = board[i][j];
+                        // replace the char with a random value
                         board[i][j] = '$';
                         
+                        // check the row, col, and the 3x3 sqaure for the same value
                         if(check(i, j, board, val) == false) return false;
+                        // place back the val
                         board[i][j] = val;
                     }
                 }
