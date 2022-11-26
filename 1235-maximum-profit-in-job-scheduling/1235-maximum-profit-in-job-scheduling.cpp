b7f4fecab9ee88b7d236a65e4ac7bde1f2309fc9
Gautam Jain <astrogeek77@outlook.com>
2022-11-26 23:55:04 +0530

    Time: 479 ms (19.32%), Space: 73.3 MB (12.55%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,84 +1,38 @@
 class Solution
 {
-    vector<int> dp;
-    struct Job
-    {
-        int st, en, profit;
-
-        Job(int st, int en, int profit)
-        {
-            this->st = st;
-            this->en = en;
-            this->profit = profit;
-        }
-
-        bool operator < (const Job &job)
-        {
-            if (st != job.st)
-            {
-                return st < job.st;
-            }
-
-            if (en != job.en)
-            {
-                return en < job.en;
-            }
-            return profit > job.profit;
-        }
-
-        bool operator==(const Job &job)
-        {
-            if (en == job.en && st == job.st && profit == job.profit)
-            {
-                return true;
-            }
-            return false;
-        }
-    };
-
     public:
-
-        int findNextJob(vector<Job> &jobs, int time, int start)
+        int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector< int > &profit)
         {
-            int end = jobs.size() - 1;
-            if (jobs[end].st < time || start > end)
-                return jobs.size();
-            while (start < end)
+            // store the start times, using map as we need the times in order.
+            map<int, int> times;
+            // {starttime -> {endtime, profit}}
+            unordered_map<int, vector<pair<int, int>>> jobs;
+            
+            // push values into the maps
+            for (auto i = 0; i < startTime.size(); ++i)
             {
-                int mid = (start + end) / 2;
-
-                if (jobs[mid].st >= time)
-                    end = mid;
-                else
-                    start = mid + 1;
+                times[startTime[i]] = 0;
+                jobs[startTime[i]].push_back({ endTime[i], profit[i] });
             }
-
-            return start;
+            // result variable
+            int max_profit = 0;
+            
+            // now we start with the biggest times
+            for (auto it = times.rbegin(); it != times.rend(); ++it)
+            {
+                // look for jobs, with starttimes
+                for (auto job: jobs[it->first])
+                {
+                    // calculate the lower bound for the end time of the job
+                    auto it = times.lower_bound(job.first);
+                    
+                    // max profit = max (current Profit, Job profit plus maximum profit at the end of the job.)
+                    max_profit = max(max_profit, (it == times.end() ? 0 : it->second) + job.second);
+                }
+                // update running profit
+                it->second = max_profit;
+            }
+            // our answer
+            return max_profit;
         }
-
-    int getMaxProfit(vector<Job> &jobs, int index)
-    {
-        if (index >= jobs.size() || index < 0)
-            return 0;
-
-        if (dp[index] != -1)
-            return dp[index];
-
-        int nextJobIndex = findNextJob(jobs, jobs[index].en, index + 1);
-        dp[index] = max(jobs[index].profit + getMaxProfit(jobs, nextJobIndex), getMaxProfit(jobs, index + 1));
-
-        return dp[index];
-    }
-
-    int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector< int > &profit)
-    {
-        vector<Job> jobs;
-        for (int i = 0; i < startTime.size(); i++)
-        {
-            jobs.push_back(Job(startTime[i], endTime[i], profit[i]));
-        }
-        dp.resize(jobs.size(), -1);
-        sort(jobs.begin(), jobs.end());
-        return getMaxProfit(jobs, 0);
-    }
 };
