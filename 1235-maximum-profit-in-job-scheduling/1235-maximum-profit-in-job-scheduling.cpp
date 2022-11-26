50a506a9d1658d8c1c80cc405313d53978ea0cee
Gautam Jain <astrogeek77@outlook.com>
2022-11-26 13:52:22 +0530

    Time: 303 ms (61.47%), Space: 50.2 MB (83.77%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,33 +1,84 @@
 class Solution
 {
-    public:
-        int dp[50001];
-    int cal(int i, int e, vector<vector < int>> &mat)
+    vector<int> dp;
+    struct Job
     {
-        if (i == mat.size()) return 0;
-        if (dp[i] != -1) return dp[i];
-        int ans = cal(i + 1, e, mat);
-        if (mat[i][0] >= e)
+        int st, en, profit;
+
+        Job(int st, int en, int profit)
         {
-            int idx = i;
-            while (idx < mat.size() && mat[idx][0] < mat[i][1]) idx++;
-            ans = max(ans, mat[i][2] + cal(idx, mat[i][1], mat));
+            this->st = st;
+            this->en = en;
+            this->profit = profit;
         }
-        return dp[i] = ans;
+
+        bool operator < (const Job &job)
+        {
+            if (st != job.st)
+            {
+                return st < job.st;
+            }
+
+            if (en != job.en)
+            {
+                return en < job.en;
+            }
+            return profit > job.profit;
+        }
+
+        bool operator==(const Job &job)
+        {
+            if (en == job.en && st == job.st && profit == job.profit)
+            {
+                return true;
+            }
+            return false;
+        }
+    };
+
+    public:
+
+        int findNextJob(vector<Job> &jobs, int time, int start)
+        {
+            int end = jobs.size() - 1;
+            if (jobs[end].st < time || start > end)
+                return jobs.size();
+            while (start < end)
+            {
+                int mid = (start + end) / 2;
+
+                if (jobs[mid].st >= time)
+                    end = mid;
+                else
+                    start = mid + 1;
+            }
+
+            return start;
+        }
+
+    int getMaxProfit(vector<Job> &jobs, int index)
+    {
+        if (index >= jobs.size() || index < 0)
+            return 0;
+
+        if (dp[index] != -1)
+            return dp[index];
+
+        int nextJobIndex = findNextJob(jobs, jobs[index].en, index + 1);
+        dp[index] = max(jobs[index].profit + getMaxProfit(jobs, nextJobIndex), getMaxProfit(jobs, index + 1));
+
+        return dp[index];
     }
+
     int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector< int > &profit)
     {
-        memset(dp, -1, sizeof(dp));
-        int n = profit.size();
-        vector<vector < int>> mat(n);
-        for (int i = 0; i < n; i++)
+        vector<Job> jobs;
+        for (int i = 0; i < startTime.size(); i++)
         {
-            mat[i] = { startTime[i],
-                endTime[i],
-                profit[i]
-            };
+            jobs.push_back(Job(startTime[i], endTime[i], profit[i]));
         }
-        sort(mat.begin(), mat.end());
-        return cal(0, 0, mat);
+        dp.resize(jobs.size(), -1);
+        sort(jobs.begin(), jobs.end());
+        return getMaxProfit(jobs, 0);
     }
 };
