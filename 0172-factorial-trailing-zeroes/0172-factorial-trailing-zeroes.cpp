deb8b9b643dd4e1307e8c61f02f0a55f13efc75b
Gautam Jain <astrogeek77@outlook.com>
2022-11-29 16:07:54 +0530

    Time: 6 ms (21.13%), Space: 5.9 MB (28.77%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,21 +1,33 @@
 class Solution
 {
     public:
+    // Approach - 3 -> O(1)
+    // int trailingZeroes(int n)
+    // {
+    //     return n / 5 + n / 25 + n / 125 + n / 625 + n / 3125;
+    // }
+
+    // O(Log N)
     int trailingZeroes(int n)
     {
-        return n / 5 + n / 25 + n / 125 + n / 625 + n / 3125;
+        int count = 0;
+        for (int i = 5; i <= n; i *= 5)
+        {
+            count += n / i;
+        }
+        return count;
     }
-    
-    // Approach - 2 O(N)
-//     int trailingZeroes(int n)
-//     {
-//         int ans = 0;
 
-//         while (n != 0)
-//         {
-//             n /= 5;
-//             ans += n;
-//         }
-//         return ans;
-//     }
+   	// Approach - 2 -> O(Log N)
+   	//     int trailingZeroes(int n)
+   	//     {
+   	//         int ans = 0;
+
+   	//         while (n != 0)
+   	//         {
+   	//             n /= 5;
+   	//             ans += n;
+   	//         }
+   	//         return ans;
+   	//     }
 };
