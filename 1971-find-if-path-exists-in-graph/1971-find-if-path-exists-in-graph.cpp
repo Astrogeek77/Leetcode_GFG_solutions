3d9d458a26939e78de3858d9fd81cf27e7234ca1
Gautam Jain <astrogeek77@outlook.com>
2022-12-19 11:00:36 +0530

    Time: 1098 ms (61.98%), Space: 244.7 MB (12.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,76 +1,30 @@
-// class Solution
-// {
-//    void DFS(vector<vector < int>> &adj, int source, vector<int> &visited)
-//     {
-//         visited[source] = true;
-//         for (auto x: adj[source])
-//         {
-//             if (!visited[x])
-//             {
-//                 DFS(adj, x, visited);
-//             }
-//         }
-//     }
-//     public:
-//         bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
-//         {
-//             vector<int> visited(n, false);
-//             vector<vector < int>> adj(n);
-
-//             for (int i = 0; i < edges.size(); i++)
-//             {
-//                 int u = edges[i][0];
-//                 int v = edges[i][1];
-
-//                 adj[u].push_back(v);
-//                 adj[v].push_back(u);
-//             }
-
-//             DFS(adj, source, visited);
-//             return visited[destination];
-//         }
-// };
 class Solution
 {
-
     public:
-        bool validPath(int n, vector<vector < int>> &edges, int src, int dest)
+    ;
+    void dfs(int node, vector<vector < int>> &edges, unordered_map<int, bool> &visited)
+    {
+        visited[node] = 1;
+
+        for (auto it: edges[node])
         {
-            vector<int> visited(n, false);
-
-            vector<vector < int>> adj(n);
-
-            for (int i = 0; i < edges.size(); i++)
+            if (!visited[it])
             {
-                int u = edges[i][0];
-                int v = edges[i][1];
-
-                adj[u].push_back(v);
-                adj[v].push_back(u);
+                dfs(it, edges, visited);
             }
-
-            queue<int> q;
-            q.push(src);
-
-            while (!q.empty())
-            {
-                for (int i = 0; i < q.size(); i++)
-                {
-                    int node = q.front();
-                    q.pop();
-
-                    if (node == dest) return true;
-
-                    visited[node] = true;
-                    for (auto x: adj[node])
-                    {
-                        if (!visited[x])
-                        {
-                            q.push(x);
-                        }
-                    }
-                }
-            }
-            return false;
         }
+    }
+    bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
+    {
+        vector<vector<int>> graph(n); // adjency list
+        unordered_map<int, bool> visited; // visited record
+        for (int i = 0; i < edges.size(); i++)
+        {
+            // bi-directional
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        dfs(source, graph, visited);
+        return visited[destination] == true;
+    }
 };
