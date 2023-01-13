bc28d34fd1bbb3b4cf2e553ab8015a0fbfbf2a43
Gautam Jain <astrogeek77@outlook.com>
2023-01-13 22:15:40 +0530

    Time: 363 ms (74.53%), Space: 117.1 MB (20.55%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,29 +1,64 @@
-class MedianFinder {
+class MedianFinder 
+{
 private:
-    priority_queue<int> maxpart;        // descending
-    priority_queue<int, vector<int>, greater<int>> minpart;    //ascending
-public:
-    MedianFinder() {
+    /* 
+        we take the stream and divide it into two list 
+        one small than the median -> smallList
+        and one greator then median -> largeList
         
-    }
+        at any time if the count is even the median will be avg of 
+        smallList->top and largeList->top
+        
+        if the numbers are odd, more numbers are stored in smallList(max Heap)
+    */
+    
+    // smallList -> maxHeap 
+    priority_queue<int> maxHeap; // smallList
+    priority_queue<int, vector<int>, greater<int>> minHeap; // largeList
+    
+public:
+    MedianFinder() {}
     
     void addNum(int num) 
-    {         
-        maxpart.push(num);              
-        minpart.push(maxpart.top());  
-        maxpart.pop();    
-        
-        if(maxpart.size() < minpart.size())
-        {   
-            maxpart.push(minpart.top());        
-            minpart.pop();                    
-        }
+    {
+        if (maxHeap.empty() or maxHeap.top() >= num)
+			maxHeap.push(num);
+		else 
+            minHeap.push(num);
+
+        // balance the two lists
+        // if smallList is larger, push the top element to largeList
+		if (maxHeap.size() > minHeap.size() + 1) 
+        {
+			minHeap.push(maxHeap.top());
+			maxHeap.pop();
+		} 
+        // if largeList is larger, push the top element to smallList
+        else if (minHeap.size() > maxHeap.size() + 1) 
+        {
+			maxHeap.push(minHeap.top());
+			minHeap.pop();
+		}
     }
     
-    double findMedian() {
-        if(maxpart.size() > minpart.size()) 
-            return maxpart.top();   
-        return (maxpart.top() + minpart.top())/2.0;  
+    double findMedian() 
+    {
+        // if the two list are of equal size
+        if (maxHeap.size() == minHeap.size()) 
+        {
+            // if empty return 0
+			if (maxHeap.empty()) return 0;
+			else 
+            {
+                // calculate avg and return avg
+				double avg = (maxHeap.top() + minHeap.top()) / 2.0;
+				return avg;
+			}
+
+		} 
+        // return top element of larger list
+        else
+			return maxHeap.size() > minHeap.size() ? maxHeap.top() : minHeap.top();
     }
 };
 /**
