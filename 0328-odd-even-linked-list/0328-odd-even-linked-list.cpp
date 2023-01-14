dc8289375681ee0dba4ca885680413405243bb1f
Gautam Jain <astrogeek77@outlook.com>
2023-01-14 14:13:26 +0530

    Time: 18 ms (66.55%), Space: 10.6 MB (32.75%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -14,15 +14,27 @@ class Solution
         ListNode* oddEvenList(ListNode *head)
         {
             if (head == NULL) return head;
-            ListNode *evenHead = head->next, *h1 = head, *h2 = head->next;
-            while (h2 && h2->next)
+            
+            // store the start of even linked list
+            ListNode *evenHead = head->next, 
+            *p1 = head, 
+            *p2 = head->next;
+            
+            // p1 tracks the odd pos list
+            // p2 tracks the even pos list
+            while (p2 && p2->next)
             {
-                h1->next = h1->next->next;
-                h2->next = h2->next->next;
-                h1 = h1->next;
-                h2 = h2->next;
+                p1->next = p1->next->next;
+                p2->next = p2->next->next;
+                p1 = p1->next;
+                p2 = p2->next;
             }
-            h1->next = evenHead;
+            // attach the even list at the end of odd list
+            
+            p1->next = evenHead;
             return head;
+            
+            // evenHead->next = p1;
+            // return evenHead;
         }
 };
