95e2b50284f512989a16c8d2524e9e4f04b6158e
Gautam Jain <astrogeek77@outlook.com>
2023-01-23 11:43:38 +0530

    Time: 214 ms (63.42%), Space: 61.5 MB (65.98%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -3,18 +3,19 @@ class Solution
     public:
         int findJudge(int n, vector<vector < int>> &trust)
         {
-
-            vector<vector < int>> g(n + 1, vector<int> (2, 0));
+            // graph[trust-connection][number of people that trust i]
+            vector<vector < int>> graph(n + 1, vector<int> (2, 0));
 
             for (int i = 0; i < trust.size(); i++)
             {
-                g[trust[i][0]][0] = 1;
-                g[trust[i][1]][1]++;
+                graph[trust[i][0]][0] = 1;
+                graph[trust[i][1]][1]++;
             }
 
             for (int i = 1; i <= n; i++)
             {
-                if (g[i][0] == 0 and g[i][1] == n - 1) return i;
+                // the town judge trusts nobody and every one trusts judge
+                if (graph[i][0] == 0 and graph[i][1] == n - 1) return i;
             }
             return -1;
         }
