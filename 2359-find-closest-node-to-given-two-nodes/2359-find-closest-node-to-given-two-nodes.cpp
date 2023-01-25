e264236e687ac9191373fdd4fccace8aac9ad86e
Gautam Jain <astrogeek77@outlook.com>
2023-01-25 11:28:16 +0530

    Time: 410 ms (41.83%), Space: 148 MB (26.04%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,31 +1,57 @@
 class Solution
 {
-    public:
-    void dfs(int node, int d, vector<int> &e, vector<int> &dist)
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &graph, int source)
     {
-        while (node != -1 and dist[node] == -1)
-        {
-            dist[node] = d++;
-            node = e[node];
-        }
-    }
-    
-    int closestMeetingNode(vector<int> &e, int node1, int node2)
-    {
-        int res = -1, min_dist = INT_MAX;
-        vector<int> dist1(e.size(), -1), dist2(e.size(), -1);
+        vector<int> dist(graph.size(), INT_MAX);
+        priority_queue<pair<int, int>, vector< pair<int, int>>, greater<pair<int, int>>> pq;
         
-        dfs(node1, 0, e, dist1);
-        dfs(node2, 0, e, dist2);
+        pq.push({ source, 0 });
+        dist[source] = 0;
         
-        for (int i = 0; i < e.size(); ++i) 
+        while (!pq.empty())
         {
-            if (min(dist1[i], dist2[i]) >= 0 and max(dist1[i], dist2[i]) < min_dist)
+            int node = pq.top().first;
+            pq.pop();
+            
+            for (pair<int, int> ngh: graph[node])
             {
-                min_dist = max(dist1[i], dist2[i]);
-                res = i;
+                int new_node = ngh.first;
+                int d = ngh.second;
+                if (dist[new_node] > dist[node] + d)
+                {
+                    dist[new_node] = dist[node] + d;
+                    pq.push({ new_node, dist[new_node] });
+                }
             }
         }
-        return res;
+        return dist;
+    }
+    public:
+    int closestMeetingNode(vector<int> &edges, int node1, int node2) 
+    {
+        vector<vector<pair<int, int>>> graph(edges.size());
+        
+        for(int i = 0; i < edges.size(); i++)
+        {
+            if(edges[i] == -1) 
+                continue;
+            
+            graph[i].push_back({edges[i], 1});
+        }
+        
+        vector<int> dist1 = dijkstra(graph, node1);
+        vector<int> dist2 = dijkstra(graph, node2);
+        
+        int min_dist = INT_MAX, min_node = -1;
+        for(int i = 0; i < dist1.size(); i++)
+        {
+            if(min_dist > max(dist1[i], dist2[i]))
+            {
+                min_dist = max(dist1[i], dist2[i]);
+                min_node = i;
+            }
+        }
+        
+        return min_node;
     }
 };
