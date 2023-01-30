91fb95501802829289f25fae55c85e3f76bbe780
Gautam Jain <astrogeek77@outlook.com>
2023-01-30 10:33:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,10 +5,12 @@ class Solution
         {
             if (n == 0) return 0;
             if (n <= 2) return 1;
-            int t1 = 0;
-            int t2 = 1;
-            int t3 = 1;
-            int t4 = 0;
+            
+            int t1 = 0; // curr - 3
+            int t2 = 1; // curr - 2
+            int t3 = 1; // curr - 1
+            
+            int t4; // curr
             for (int i = 3; i <= n; i++)
             {
                 t4 = t1 + t2 + t3;
