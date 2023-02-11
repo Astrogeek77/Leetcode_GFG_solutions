9529daebf63294921067b088a4ee01a5b3ab88a0
Gautam Jain <astrogeek77@outlook.com>
2023-02-11 10:51:31 +0530

    Time: 33 ms (24.81%), Space: 15.5 MB (40.66%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -7,48 +7,51 @@ class Solution
             vector<vector<pair<int, int>>> adj(n);
             for (auto it: redEdges)
             {
-                adj[it[0]].push_back({ it[1],
-                    0 });	// 0 -> red
+                adj[it[0]].push_back({ it[1], 0 });	// 0 -> red
             }
             for (auto it: blueEdges)
             {
-                adj[it[0]].push_back({ it[1],
-                    1 });	// 1 -> blue
+                adj[it[0]].push_back({ it[1], 1 });	// 1 -> blue
             }
+            
+            
             dist[0] = 0;
             queue<vector < int>> q;
-           	//2 is only for the first node
-            q.push({ 0,
-                0,
-                2 });	//node,dist and color(Here color is 2 just for the source)
+           	//2 is only for the src / first node
+            q.push({ 0, 0, 2 });	//node, dist and color(Here color is 2 just for the source)
+            
             bool vis[101][3];
             memset(vis, false, sizeof(vis));
+            
             while (!q.empty())
             {
-                auto it = q.front();
+                vector<int> it = q.front();
                 q.pop();
-                int cur = it[0];
-                int dis = it[1];
-                int edge = it[2];
-                for (auto nei: adj[cur])
+                
+                int curNode = it[0];
+                int d = it[1];
+                int color = it[2];
+                
+                for (auto ngh: adj[curNode])
                 {
-                    if (vis[nei.first][nei.second] == true or nei.second == edge) continue;
-                    if (dist[nei.first] == -1)
+                    int nghNode = ngh.first;
+                    int nghColor = ngh.second;
+                        
+                    // already visited node or node with same color as parent node    
+                    if (vis[nghNode][nghColor] == true or nghColor == color) continue;
+                    
+                    else if (dist[nghNode] == -1 and !vis[nghNode][nghColor])
                     {
-                    	//if it is -1 then we are definately visiting it for the first time so lets update its value
-                        dist[nei.first] = dis + 1;
-                        vis[nei.first][nei.second] = true;
-                        q.push({ nei.first,
-                            dis + 1,
-                            nei.second });
+                    	//if dist is -1 then we are definately visiting it for the first time so lets update its value
+                        dist[nghNode] = d + 1;
+                        vis[nghNode][nghColor] = true;
+                        q.push({ nghNode, d + 1, nghColor });
                     }
                     else
                     {
                     	//if the dist[nei.first] != -1 then the shortest distance has already been found and makes no sense to update it again
-                        vis[nei.first][nei.second] = true;
-                        q.push({ nei.first,
-                            dis + 1,
-                            nei.second });
+                        vis[nghNode][nghColor] = true;
+                        q.push({ nghNode, d + 1, nghColor });
                     }
                 }
             }
