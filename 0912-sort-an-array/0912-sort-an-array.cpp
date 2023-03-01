5f64a4f04fe680c4fcf3a6da89ba9bdca3f66eae
Gautam Jain <astrogeek77@outlook.com>
2023-03-01 16:19:03 +0530

    Time: 280 ms (39.38%), Space: 61.6 MB (52.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,42 +1,190 @@
 class Solution
 {
-   	// heapify util for heap sort
-    void heapify(vector<int> &arr, int n, int i)
+//     void printarray(vector<int> &v, int start, int end, int tabs, string type, int d)
+//     {
+
+//         int i, j, n = v.size(), t = tabs;
+
+//         while (t--) cout << "|\t\t";
+//         for (i = 0; i < 50; i++) cout << "~";
+//         cout << endl;
+
+//         if (type != "output")
+//         {
+//             t = tabs;
+//             while (t--) cout << "|\t\t";
+//             cout << endl;
+//         }
+
+//         t = tabs;
+//         while (t--) cout << "|\t\t";
+//         cout << type << " (depth: " << d << ")" << endl;
+
+//         t = tabs;
+//         while (t--) cout << "|\t\t";
+//         for (i = 0; i < start; i++) cout << "- ";
+//         for (i = start; i <= end; i++) cout << v[i] << " ";
+//         for (i = end + 1; i < n; i++) cout << "- ";
+//         cout << endl;
+
+//         if (type == "output")
+//         {
+//             t = tabs;
+//             while (t--) cout << "|\t\t";
+//             cout << endl;
+//         }
+
+//         return;
+//     }
+
+    void max_heap(vector<int> &a, int i, int x, int y)
     {
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-        
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-        
-        if (largest != i)
+        int l = i;
+
+        if ((2 *i) + 1 - x < (y + 1) && a[(2 *i) + 1 - x] > a[l])
+            l = (2 *i) + 1 - x;
+        if ((2 *i) + 2 - x < (y + 1) && a[(2 *i) + 2 - x] > a[l])
+            l = (2 *i) + 2 - x;
+
+        if (l != i)
         {
-            swap(arr[i], arr[largest]);
-            heapify(arr, n, largest);
+            a[i] = a[l] + a[i];
+            a[l] = a[i] - a[l];
+            a[i] = a[i] - a[l];
+            max_heap(a, l, x, y);
         }
     }
 
-   	// heap sort
-    void heapSort(vector<int> &arr)
+    void build_heap(vector<int> &a, int x, int y)
     {
-        int n = arr.size();
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
-        for (int i = n - 1; i >= 0; i--)
+        int p = (x + y) / 2;
+        for (int i = p; i >= x; i--)
+            max_heap(a, i, x, y);
+    }
+
+    void heapsort(vector<int> &a, int x, int y)
+    {
+        build_heap(a, x, y);
+        int len = y;
+        for (int i = y; i >= x + 1; i--)
         {
-            swap(arr[0], arr[i]);
-            heapify(arr, i, 0);
+            a[x] = a[x] + a[i];
+            a[i] = a[x] - a[i];
+            a[x] = a[x] - a[i];
+            len--;
+            max_heap(a, x, x, len);
         }
     }
-    
-    public:
-    vector<int> sortArray(vector<int> &nums) 
+
+    void insertionsort(vector<int> &a, int x, int y)
     {
-        heapSort(nums);
+        int temp, j;
+        for (int i = x + 1; i <= y; i++)
+        {
+            temp = a[i];
+            j = i - 1;
+
+            while (j >= x && a[j] > temp)
+            {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = temp;
+        }
+    }
+
+    int get_pivot(vector<int> &a, int x, int y)
+    {
+        int m = (x + y) / 2;
+
+        if (a[m] < a[x])
+        {
+            a[x] = a[x] + a[m];
+            a[m] = a[x] - a[m];
+            a[x] = a[x] - a[m];
+        }
+
+        if (a[y] < a[m])
+        {
+            a[y] = a[y] + a[m];
+            a[m] = a[y] - a[m];
+            a[y] = a[y] - a[m];
+
+            if (a[m] < a[x])
+            {
+                a[x] = a[x] + a[m];
+                a[m] = a[x] - a[m];
+                a[x] = a[x] - a[m];
+            }
+        }
+
+        a[x] = a[x] + a[m];
+        a[m] = a[x] - a[m];
+        a[x] = a[x] - a[m];
+
+        return x;
+    }
+
+    int partition(vector<int> &a, int x, int y)
+    {
+        int l = get_pivot(a, x, y);
+
+        while (true)
+        {
+            while (a[l] <= a[y] && l != y)
+                y--;
+            if (l == y)
+                return l;
+
+            a[l] = a[l] + a[y];
+            a[y] = a[l] - a[y];
+            a[l] = a[l] - a[y];
+            l = y;
+
+            while (a[l] >= a[x] && l != x)
+                x++;
+            if (l == x)
+                return l;
+
+            a[l] = a[l] + a[x];
+            a[x] = a[l] - a[x];
+            a[l] = a[l] - a[x];
+            l = x;
+        }
+    }
+
+    void introsort(vector<int> &a, int x, int y, int d, int steps, int tabs)
+    {
+        int l = y - x + 1, m, i;
+
+        if (l < 5)
+        {
+            // printarray(a, x, y, tabs, "insertion", d);
+            insertionsort(a, x, y);
+        }
+        else if (d == 0)
+        {
+            // printarray(a, x, y, tabs, "heapsort", d);
+            heapsort(a, x, y);
+        }
+        else
+        {
+            // printarray(a, x, y, tabs, "quicksort", d);
+            m = partition(a, x, y);
+            introsort(a, x, m - 1, d - 1, ++steps, tabs + 1);
+            introsort(a, m + 1, y, d - 1, ++steps, tabs + 1);
+        }
+        // printarray(a, x, y, tabs, "output", d);
+    }
+    public:
+    vector<int> sortArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        
+        int d = 2 * floor(log(n));
+        introsort(nums, 0, n - 1, d, 0, 0);
+        
         return nums;
     }
 };
