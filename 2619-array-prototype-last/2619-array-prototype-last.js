8fe5d8f350bcd23e2725b4ed1c4fb275e4e48d7f
Gautam Jain <astrogeek77@outlook.com>
2023-05-27 09:47:38 +0530

    Time: 57 ms (56.54%), Space: 41.9 MB (46.36%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,9 +1,3 @@
-declare global {
-    interface Array<T> {
-        last(): T | -1;
-    }
-}
-
 Array.prototype.last = function() {
     if(!this.length) return -1
 
@@ -14,5 +8,3 @@ Array.prototype.last = function() {
  * const arr = [1, 2, 3];
  * arr.last(); // 3
  */
-
-export {};
